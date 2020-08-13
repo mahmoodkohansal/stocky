23f4de79ed6d93fd16d10eefcff234c33a9d981c
Mahmood Kohansal <m.kohansal@azki.com>
2020-08-13 12:31:50 +0430

    list of all stocks api modified to handle queryParams

--- src/main/java/ir/mahmood/sahame/controller/StockController.java
+++ src/main/java/ir/mahmood/sahame/controller/StockController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,8 +65,8 @@ public class StockController {
     }
 
     @GetMapping("/")
-    public Page<StockDto> listStocks() {
-        Pageable pageable = PageRequest.of(0, 10);
+    public Page<StockDto> listStocks(Pageable pageable) {
+        log.info(pageable);
         return stockService.list(pageable);
     }
 
